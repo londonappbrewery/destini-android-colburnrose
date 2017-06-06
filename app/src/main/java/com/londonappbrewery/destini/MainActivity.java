a80201e722d1220701480a99c8fbe32e1c1ad061
colburnrose <colburnsanders@gmail.com>
2017-06-06 11:52:42 -0500

    made the connection to detect clicks and created the debug log within the buttons.

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,6 +2,8 @@ package com.londonappbrewery.destini;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,15 +20,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
         mStoryView = (TextView) findViewById(R.id.storyTextView);
         mButtonTop = (Button) findViewById(R.id.buttonTop);
         mButtonBottom = (Button) findViewById(R.id.buttonBottom);
-
-
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
+        mButtonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Main Activity", "True Button pressed!");
+            }
+        });
+        mButtonBottom.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.d("Main Activity", "False Button pressed!");
+            }
+        });
 
 
 
