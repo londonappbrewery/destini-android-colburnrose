471072413904dfdb6c334de014e3bc6920d5dbb0
colburnrose <colburnsanders@gmail.com>
2017-06-08 13:03:23 -0500

    testing the apps functionality of the story line, made some progress with the implementation of the updateStory method

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,7 +2,6 @@ package com.londonappbrewery.destini;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,9 +9,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-    TextView mStoryView;
+    TextView mStoryText;
     Button mButtonTop;
     Button mButtonBottom;
+    int mStoryId =1;
 
 
     @Override
@@ -21,24 +21,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-        mStoryView = (TextView) findViewById(R.id.storyTextView);
+        mStoryText = (TextView) findViewById(R.id.storyTextView);
         mButtonTop = (Button) findViewById(R.id.buttonTop);
         mButtonBottom = (Button) findViewById(R.id.buttonBottom);
+
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
         mButtonTop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Main Activity", "True Button pressed!");
+                updateStory(mStoryText.getText(), true);
             }
         });
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-        mButtonBottom.setOnClickListener(new View.OnClickListener(){
+        mButtonBottom.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Log.d("Main Activity", "False Button pressed!");
+            public void onClick(View v) {
+                updateStory(mStoryText.getText(), false);
             }
-        });
 
+        });
+    }
+
+    public void updateStory(CharSequence text, Boolean answer) {
+        if (text == getString(R.string.T1_Story)) {
+            if (answer == true) {
+                mStoryText.setText(R.string.T3_Story);
+                mButtonTop.setText(R.string.T3_Ans1);
+                mButtonBottom.setText(R.string.T3_Ans2);
+            }
+            else {
+                mStoryText.setText(R.string.T2_Story);
+                mButtonTop.setText(R.string.T2_Ans1);
+                mButtonBottom.setText(R.string.T2_Ans2);
+            }
+        }
+        else if(text == getString(R.string.T2_Story)){
+            if(answer == true){
+                mStoryText.setText(R.string.T3_Story);
+                mButtonTop.setText(R.string.T3_Ans1);
+                mButtonBottom.setText(R.string.T3_Ans2);
+            }
+            else {
+                mStoryText.setText(R.string.T2_Story);
+                mButtonTop.setText(R.string.T2_Ans1);
+                mButtonBottom.setText(R.string.T2_Ans2);
+            }
+        }
     }
 }
+
